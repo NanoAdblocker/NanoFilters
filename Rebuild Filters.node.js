a4b138a3a3ddc8c643a3bf77947175ec8975a0b4
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-05-23 11:18:42 -0600

    rebuild files

--- Rebuild Filters.node.js
+++ Rebuild Filters.node.js
@@ -3,6 +3,7 @@
  */
 "use strict";
 
+
 /**
  * The file system modules.
  * @const {Module}
@@ -17,6 +18,12 @@ const fs = (() => {
     };
 })();
 
+
+/**
+ * New line splitter.
+ * @const {RegExp}
+ */
+const reNewLine = /\r\n|\n|\r/;
 /**
  * Minimize assets.json.
  * @function
@@ -32,12 +39,12 @@ const minimizeMeta = (raw) => {
 /**
  * Minimize the Public Suffix List.
  * @function
+ * @param {string} raw - The raw data.
  * @param {string} license - The link to the license.
  * @param {string} source - The link to the source.
- * @param {string} raw - The raw data.
  * @return {string} The minimized data.
  */
-const minimizePSL = (license, source, raw) => {
+const minimizePSL = (raw, license, source) => {
     raw = raw.split(reNewLine);
 
     let out = [
@@ -67,12 +74,12 @@ const minimizePSL = (license, source, raw) => {
 /**
  * Minimize a resource file.
  * @function
+ * @param {string} raw - The raw data.
  * @param {string} license - The link to the license.
  * @param {string} source - The link to the source.
- * @param {string} raw - The raw data.
  * @return {string} The minimized data.
  */
-const minimizeResource = (name, license, source, raw) => {
+const minimizeResource = (raw, license, source) => {
     raw = raw.split(reNewLine);
 
     let out = [
@@ -107,14 +114,14 @@ const minimizeResource = (name, license, source, raw) => {
 /**
  * Minimize a filter list.
  * @function
+ * @param {string} raw - The raw data.
  * @param {string} title - The title of the filter.
  * @param {string} license - The link to the license.
  * @param {string} source - The link to the source map.
  * @param {integer} expires - The update interval in days.
- * @param {string} raw - The raw data.
  * @return {string} The minimized data.
  */
-const minimizeFilter = (title, license, source, expires, raw) => {
+const minimizeFilter = (raw, title, license, source, expires) => {
     raw = raw.split(reNewLine);
 
     let out = [
@@ -135,7 +142,7 @@ const minimizeFilter = (title, license, source, expires, raw) => {
             // https://github.com/AdguardTeam/AdguardBrowserExtension/issues/917
             continue;
         }
-        if (f.startsWith("# ")) {
+        if (f.startsWith("# ") || f === "#") {
             continue;
         }
 
@@ -148,149 +155,140 @@ const minimizeFilter = (title, license, source, expires, raw) => {
 /**
  * Minimize a hosts file.
  * @function
+ * @param {string} raw - The raw data.
  * @param {string} title - The title of the filter.
  * @param {string} license - The link to the license.
  * @param {string} source - The link to the source map.
  * @param {integer} expires - The update interval in days.
- * @param {string} raw - The raw data.
  * @param {boolean} [abp_compat=true] - Whether Adblock Plus compatible filter
  * should be generated.
  * @return {string} The minimized data.
  */
-const minimizeHosts = (title, license, source, expires, raw, abp_compat = true) => {
-    raw = raw.split(reNewLine);
+const minimizeHosts = (() => {
+    const reWhitespace = /\s+/;
 
-    let out = [
-        "[Adblock Plus 3.0]",
-        "! Title: " + title,
-        "! License: " + license,
-        "! Source: " + source,
-        "! Expires: " + expires.toString() + " day",
-        "! Cached: " + (new Date()).toString(),
-    ];
-    for (let f of raw) {
-        const i = f.indexOf("#");
-        if (i > -1) {
-            f = f.substring(0, i);
-        }
+    const localTest1 = new Set([
+        // reIsLocalhostRedirect
+        "0.0.0.0",
+        "broadcasthost",
+        "localhost",
+        "local",
 
-        f = f.trim();
+        // reLocalIp
+        "127.0.0.1",
+        "::1",
+        "fe80::1%lo0",
+    ]);
+    const localTest2 = /^ip6-\w+$/;
 
-        if (f.length === 0) {
-            continue;
-        }
+    return (raw, title, license, source, expires, abp_compat = true) => {
+        raw = raw.split(reNewLine);
 
-        f = f.split(reWhitespace);
-        for (let d of f) {
-            d = d.trim();
+        let out = [
+            "[Adblock Plus 3.0]",
+            "! Title: " + title,
+            "! License: " + license,
+            "! Source: " + source,
+            "! Expires: " + expires.toString() + " day",
+            "! Cached: " + (new Date()).toString(),
+        ];
+        for (let f of raw) {
+            const i = f.indexOf("#");
+            if (i > -1) {
+                f = f.substring(0, i);
+            }
 
-            if (d.length === 0) {
+            f = f.trim();
+
+            if (f.length === 0) {
                 continue;
             }
-            if (hostsBlacklist.has(d)) {
-                continue;
+
+            f = f.split(reWhitespace);
+            for (let d of f) {
+                d = d.trim();
+
+                if (d.length === 0) {
+                    continue;
+                }
+                if (localTest1.has(d) || localTest2.test(d)) {
+                    continue;
+                }
+
+                if (abp_compat) {
+                    out.push("||" + d + "^");
+                } else {
+                    out.push(d);
+                }
             }
-
-            if (abp_compat) {
-                out.push("||" + d + "^");
-            } else {
-                out.push(d);
-            }
         }
-    }
-    out.push("");
-
-    return out.join("\n");
-};
-
-/**
- * Process a filter list.
- * @async @function
- * @param {string} name - The name of the filter to process.
- * @param {string} license - The link to the license.
- * @param {string} srcmap - The link to the source map.
- */
-const processFilter = async (name, license, srcmap) => {
-    let data = await fs.readFile("NanoFiltersSource/" + name, "utf8");
-    data = data.split(/\r\n|\n|\r/g);
-
-    let out = [
-        "!@pragma nano-license=" + license,
-        "!@pragma nano-srcmap=" + srcmap,
-    ];
-    for (let f of data) {
-        f = f.trim();
-
-        if (f.length === 0) {
-            continue;
-        }
-        if (f.startsWith("!") && !f.startsWith("!#")) {
-            // https://github.com/AdguardTeam/AdguardBrowserExtension/issues/917
-            continue;
-        }
-        if (f.startsWith("# ")) {
-            continue;
-        }
-
-        out.push(f);
-    }
-    out.push("");
-
-    await fs.writeFile("NanoFilters/" + name, out.join("\n"), "utf8");
-};
-/**
- * Process a resource file.
- * @async @function
- * @param {string} name - The name of the resource to process.
- * @param {string} license - The link to the license.
- * @param {string} srcmap - The link to the source map.
- */
-const processResource = async (name, license, srcmap) => {
-    let data = await fs.readFile("NanoFiltersSource/" + name, "utf8");
-    data = data.split(/\r\n|\n|\r/g);
-
-    let out = [
-        "# License: " + license,
-        "# Source: " + srcmap,
-    ];
-    let lastLineEmpty = false;
-    for (let l of data) {
-        // Must copy the line
-        let line = l.trim();
-
-        if (line.startsWith("# ")) {
-            continue;
-        }
-
-        if (line.length === 0) {
-            if (!lastLineEmpty) {
-                lastLineEmpty = true;
-                out.push("");
-            }
-            continue;
-        }
-
-        lastLineEmpty = false;
-        out.push(l);
-    }
-    if (!lastLineEmpty) {
         out.push("");
-    }
 
-    await fs.writeFile("NanoFilters/" + name, out.join("\n"), "utf8");
+        return out.join("\n");
+    };
+})();
+
+
+/**
+ * Process one file.
+ * @async @function
+ * @param {string} inFile - The path to input file.
+ * @param {string} outFile - The path to output file.
+ * @param {Function} handler - The minimizer.
+ * @param {any} args - Arguments to pass into the minimizer.
+ */
+const processOne = async (inFile, outFile, handler, ...args) => {
+    const data = await fs.readFile(inFile, "utf8");
+    await fs.writeFile(outFile, handler(data, ...args), "utf8");
 };
+/**
+ * Process one Nano Filter.
+ * @async @function
+ * @param {string} name - The name of the filter.
+ * @param {string} title - The title of the filter.
+ */
+const processOneNanoFilter = async (name, title) => {
+    await processOne(
+        "./NanoFiltersSource/" + name,
+        "./NanoFilters/" + name,
+        minimizeFilter,
+        title,
+        "GPL-3.0",
+        "https://github.com/NanoAdblocker/NanoFilters/tree/master/NanoFiltersSource/" + name,
+        1,
+    );
+};
+/**
+ * Process one resource.
+ * @async @function
+ * @param {string} inDir - The input directory.
+ * @param {string} outDir - The output directory.
+ * @param {string} name - The file name.
+ */
+const processOneResource = async (inDir, outDir, name) => {
+    await processOne(
+        "./" + inDir + "/" + name,
+        "./" + outDir + "/" + name,
+        minimizeResource,
+        "GPL-3.0",
+        "https://github.com/NanoAdblocker/NanoFilters/tree/master/" + inDir + "/" + name,
+    );
+};
+
+
+process.on("unhandledRejection", (err) => {
+    throw err;
+});
 
 (async () => {
-    const NanoFiltersLicense = "https://github.com/NanoAdblocker/NanoFilters/blob/master/LICENSE";
-    const NanoFiltersSrcMapPrefix = "https://raw.githubusercontent.com/NanoAdblocker/NanoFilters/master/NanoFiltersSource/";
     await Promise.all([
-        processFilter("NanoBase.txt", NanoFiltersLicense, NanoFiltersSrcMapPrefix + "NanoBase.txt"),
-        processFilter("NanoAnnoyance.txt", NanoFiltersLicense, NanoFiltersSrcMapPrefix + "NanoAnnoyance.txt"),
-        processFilter("NanoTimer.txt", NanoFiltersLicense, NanoFiltersSrcMapPrefix + "NanoTimer.txt"),
-        processFilter("NanoWhitelist.txt", NanoFiltersLicense, NanoFiltersSrcMapPrefix + "NanoWhitelist.txt"),
+        processOneNanoFilter("NanoBase.txt", "Nano filters"),
+        processOneNanoFilter("NanoAnnoyance.txt", "Nano filters - Annoyance"),
+        processOneNanoFilter("NanoWhitelist.txt", "Nano filters - Whitelist"),
 
-        processResource("NanoResources.txt", NanoFiltersLicense, NanoFiltersSrcMapPrefix + "NanoWhitelist.txt"),
+        processOneResource("NanoFiltersSource", "NanoFilters", "NanoResources.txt"),
     ]);
 
     console.log("Done");
 })();
+

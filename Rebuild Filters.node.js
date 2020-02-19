90228786ee129c96c4bab7a51b2515854f30acef
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2020-02-19 11:00:43 -0700

    No commit message

--- Rebuild Filters.node.js
+++ Rebuild Filters.node.js
@@ -217,8 +217,10 @@ const minimizeHosts = (() => {
         "local",
 
         // reLocalIp
+        "0",
         "0.0.0.0",
         "127.0.0.1",
+        "::",
         "::1",
         "fe80::1%lo0",
     ]);
